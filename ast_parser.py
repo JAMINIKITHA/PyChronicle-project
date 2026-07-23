d4a77785d80f5b13dd27ac4a1e05c5747f9657f1
JAMI NIKITHA <jaminikitha35@gmail.com>
2026-07-23 15:20:40 +0530

    Added import extraction using AST

--- ast_parser.py
+++ ast_parser.py
@@ -1,5 +1,8 @@
 import ast
 
+print("AST Parser Running")
+
+
 def parse_python_file(file_path):
     with open(file_path, "r") as file:
         tree = ast.parse(file.read())
@@ -9,6 +12,7 @@ def parse_python_file(file_path):
 def extract_details(tree):
     functions = []
     classes = []
+    imports = []
 
     for node in ast.walk(tree):
         if isinstance(node, ast.FunctionDef):
@@ -17,10 +21,22 @@ def extract_details(tree):
         elif isinstance(node, ast.ClassDef):
             classes.append(node.name)
 
+        elif isinstance(node, ast.Import):
+            for item in node.names:
+                imports.append(item.name)
+
+        elif isinstance(node, ast.ImportFrom):
+            imports.append(node.module)
+
     print("Functions:", functions)
     print("Classes:", classes)
+    print("Imports:", imports)
 
+
+print("Tree created")
 
 tree = parse_python_file("sample.py")
+
+print("Calling extractor")
 
 extract_details(tree)
