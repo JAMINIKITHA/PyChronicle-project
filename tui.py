363ea7a9868be39834dd2b38fa7bfbc0a17ccdc5
JAMI NIKITHA <jaminikitha35@gmail.com>
2026-07-26 13:23:00 +0530

    Commit 25 - Added watch variables panel and finalized terminal UI

--- tui.py
+++ tui.py
@@ -8,14 +8,20 @@ class PyChronicleApp(App):
 
     CSS = """
     #code_view {
-        width: 70%;
+        width: 60%;
         border: solid green;
         padding: 1;
     }
 
     #timeline {
-        width: 30%;
+        width: 20%;
         border: solid yellow;
+        padding: 1;
+    }
+
+    #watch {
+        width: 20%;
+        border: solid cyan;
         padding: 1;
     }
     """
@@ -34,27 +40,41 @@ class PyChronicleApp(App):
         for i, line in enumerate(code, start=1):
             code_text += f"{i:3} | {line}"
 
-        # Load database info
+        # Load database
         conn = sqlite3.connect("pychronicle.db")
         cursor = conn.cursor()
 
         cursor.execute("SELECT COUNT(*) FROM variable_history")
         total_frames = cursor.fetchone()[0]
 
+        cursor.execute("""
+            SELECT variable_name, variable_value
+            FROM variable_history
+            ORDER BY id DESC
+            LIMIT 10
+        """)
+
+        variables = cursor.fetchall()
         conn.close()
 
         timeline_text = (
-            "⏳ Timeline\n\n"
-            f"Total Frames : {total_frames}\n\n"
-            "Current Frame : 1\n\n"
-            "Status : Ready"
+            f"⏳ Timeline\n\n"
+            f"Frames : {total_frames}\n\n"
+            f"Current : Latest\n\n"
+            f"Status : Ready"
         )
+
+        watch_text = "👀 Watch Variables\n\n"
+
+        for name, value in variables:
+            watch_text += f"{name} = {value}\n"
 
         yield Header()
 
         with Horizontal():
             yield Static(code_text, id="code_view")
             yield Static(timeline_text, id="timeline")
+            yield Static(watch_text, id="watch")
 
         yield Footer()
 
